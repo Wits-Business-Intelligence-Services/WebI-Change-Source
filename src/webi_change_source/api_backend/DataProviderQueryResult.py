566027cd6cd41b5d466c74111c64a15f21dd8139
Kyle Weiher <kyle.weiher@wits.ac.za>
2026-08-27 16:01:30 +0200

    Update DataProviderQueryResult.py to use new styling and remove unused __str__ method

--- src/webi_change_source/api_backend/DataProviderQueryResult.py
+++ src/webi_change_source/api_backend/DataProviderQueryResult.py
@@ -8,11 +8,10 @@ class DataProviderQueryResult:
     id: str
     name: str
 
-    def print(self):
+    def print(
+        self,
+    ):
         print(f"name: {self.name}")
         print(f"data_source_id: {self.data_source_id}")
         print(f"data_source_type: {self.data_source_type}")
         print(f"id: {self.id}")
-
-    def __str__(self):
-        return f"{self.name}|||{self.id}|||{self.data_source_type}|||{self.data_source_id}"
